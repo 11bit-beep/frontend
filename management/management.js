a780c16cc458fe1d6831c8f399d07a2c5f03d42b
mingi-cpu <a01062474215@gmail.com>
2026-08-24 21:17:08 +0900

    feat:반별,실변상세조회 구현, 페이지로 분리

--- management/management.js
+++ management/management.js
@@ -1,9 +1,1 @@
-const checkClass = document.querySelector("#checkClass");
-const checkRoom = document.querySelector("#checkRoom");
-const main = document.querySelector("main");
-const onClickClass=(e)=>{
-    main.innerHTML=``
-}
-const onClickRoom=(e)=>{
-    main.innerHTML=``
-}
+
