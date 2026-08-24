b9d6b45c0d7c9838972aa6a7b73dd2efa9440f00
mingi-cpu <a01062474215@gmail.com>
2026-08-24 17:31:12 +0900

    feat:반별,실별조회 뷰 구현

---
+++ management/management.js
@@ -0,0 +1,9 @@
+const checkClass = document.querySelector("#checkClass");
+const checkRoom = document.querySelector("#checkRoom");
+const main = document.querySelector("main");
+const onClickClass=(e)=>{
+    main.innerHTML=``
+}
+const onClickRoom=(e)=>{
+    main.innerHTML=``
+}
