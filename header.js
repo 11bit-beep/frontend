261181d56ae3512b2c65b1ef84ab9c7b7072b311
kdh100506 <kdh100506@dgsw.hs.kr>
2026-08-25 11:51:40 +0900

    feat: 프론트엔드 전체 파일 업로드

---
+++ header.js
@@ -0,0 +1,12 @@
+const headerLink = document.querySelector('header .linkWrap a');
+const headerLinkIcon = document.querySelector('header .linkWrap a path');
+const homeLink = document.querySelector('.homeLink');
+const managementLink = document.querySelector('.managementLink');
+const windowLocateion = window.location.pathname;
+if (window.location.pathname ==='/management/management.html'){
+    managementLink.id='nowLink';
+    homeLink.id ='';
+}else if (window.location.pathname === '/main/main.html'){
+    homeLink.id='nowLink';
+    managementLink.id='';
+}
